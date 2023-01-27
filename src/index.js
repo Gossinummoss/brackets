99413e91fe8e6780c1c3b5683ec9319528fc2f9e
Gossinummoss <gossinummoss@gmail.com>
2023-01-27 06:37:16 +0500

    feat: almost done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-    const stack = [];
+  var stack = [];
 
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < bracketsConfig.length; j++) {
-      if (str[i] === bracketsConfig[j][0] || str[i] === '|') {
-        stack.push(str[i]);
-        break;
-      } else if (str[i] === bracketsConfig[j][1]) {
-          if (stack[stack.length - 1] === '|') {
-              stack.splice(-2);
-              break;
+for (let i = 0; i < str.length; i++) {
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    if (str[i] === bracketsConfig[j][0] || str[i] === '|') {
+      stack.push(str[i]);
+      break;
+    } else if (str[i] === bracketsConfig[j][1]) {
+        if (stack[stack.length - 1] === '|') {
+            var result = stack.reduce(function(acc, el) {
+          acc[el] = (acc[el] || 0) + 1;
+          return acc;
+          }, {});
+          if (result['|'] % 2 === 0 && str.length > 4) {
+              var stack = stack.join('').split('|').join('').split('');
           }
-        if (stack.length === 0 || stack[stack.length - 1] !== bracketsConfig[j][0]) {
-          return false;
-        } else {
-          stack.pop();
-          break;
         }
+      if (stack.length === 0 || stack[stack.length - 1] !== bracketsConfig[j][0]) {
+        return false;
+      } else {
+        stack.pop();
+        break;
       }
     }
   }
+}
   
   if (stack[0] === '(' || stack[0] === ')') {
       return stack.length === 0
